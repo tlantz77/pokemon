bbb19a8b7968fac76ead3724e04af20210061e3a
Tim Lantz <tlantz@gmail.com>
2016-06-10 10:12:53 -0500

    started ajax

--- public/js/application.js
+++ public/js/application.js
@@ -6,4 +6,22 @@ $(document).ready(function() {
 
     $(this).text("Hello");
   });
+
+  $('#poke-form').on('submit', function(event) {
+    event.preventDefault;
+    var $form = $(this);
+    url = $form.attr('action');
+    method = $form.attr('method');
+    data = $form.serialize();
+
+    $.ajax({
+      url: url,
+      method: method,
+      data: data
+    })
+
+      .done(function(response) {
+
+      })
+  })
 });
